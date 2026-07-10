ecbbe15f3ddc41a37aaca10c980c428703d64f5f
Tebie Tegenew <innovator1227@gmail.com>
2026-07-10 16:02:30 +0300

    new file:   Admin/.gitignore 	new file:   Admin/README.md 	new file:   Admin/eslint.config.js 	new file:   Admin/index.html 	new file:   Admin/package-lock.json 	new file:   Admin/package.json 	new file:   Admin/public/favicon.svg 	new file:   Admin/public/icons.svg 	new file:   Admin/src/App.css 	new file:   Admin/src/App.jsx 	new file:   Admin/src/assets/hero.png 	new file:   Admin/src/assets/react.svg 	new file:   Admin/src/assets/vite.svg 	new file:   Admin/src/index.css 	new file:   Admin/src/main.jsx 	new file:   Admin/vite.config.js

---
+++ Admin/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App =
+ () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100 text-shadow-black">
+      Admin application
+    </div>
+  )
+}
+
+export default App
